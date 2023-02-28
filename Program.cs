683992ffda413b8bcc95fd09cb73b3f2e6b5dcf5
princeakhoury13 <princepraveen130509@gmail.com>
2023-02-28 19:09:38 +0530

    added: Check for anagrams

--- Program.cs
+++ Program.cs
@@ -4,15 +4,8 @@
     {
         public static void Main(string[] args)
         {
-            int[] arr = { 52, 4, 300, 21, 102 };
-
-            Console.WriteLine("Array before sorting:");
-            Sorting.PrintArray(arr);
-
-            Sorting.MergeSort(arr, 0, arr.Length - 1);
-
-            Console.WriteLine("\nArray after sorting:");
-            Sorting.PrintArray(arr);
+            Sorting ana = new Sorting();
+            ana.Display();
         }
     }
 }
