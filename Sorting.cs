f35c5999e260f75a61674e11b139be20f88884b3
princeakhoury13 <princepraveen130509@gmail.com>
2023-02-28 18:47:44 +0530

    added: Logic for Bubble sorting

--- Sorting.cs
+++ Sorting.cs
@@ -11,51 +11,46 @@ namespace Day16And17
         public void Display()
         {
 
-            int[] arr1 = {};
+            int[] arr = { };
             int size;
             Console.WriteLine("Enter size of the array");
             size = Convert.ToInt32(Console.ReadLine());
-            arr1 = new int[size];
+            arr = new int[size];
 
             for (int k = 0; k < size; k++)
             {
                 int number = k + 1;
-                Console.WriteLine("Enter element number: "+number);
-                arr1[k] = Convert.ToInt32(Console.ReadLine());
-            }
-            
-            int  i, j, val, flag;
-
-            Console.WriteLine("Initial Array");
-            for (i = 0; i < size; i++)
-            {
-                Console.WriteLine(arr1[i]);
+                Console.WriteLine("Enter element number: " + number);
+                arr[k] = Convert.ToInt32(Console.ReadLine());
             }
 
-            for (i = 1; i < size; i++)
+            Console.WriteLine("Given Array :");
+            foreach (int ele in arr)
             {
-                val = arr1[i];
-                flag = 0;
-                for (j = i - 1; j >= 0 && flag != 1;)
+                Console.WriteLine(ele);
+            }
+            int temp;
+
+            for (int j = 0; j <= arr.Length - 2; j++)
+            {
+                for (int i = 0; i <= arr.Length - j - 2; i++)
                 {
-                    if (val < arr1[j])
+                    if (arr[i] > arr[i + 1])
                     {
-                        arr1[j + 1] = arr1[j];
-                        j--;
-                        arr1[j + 1] = val;
-                    }
-                    else
-                    {
-                        flag = 1;
+                        temp = arr[i + 1];
+                        arr[i + 1] = arr[i];
+                        arr[i] = temp;
                     }
                 }
             }
-            Console.WriteLine("Sorted Array: ");
-            for (i = 0; i < size; i++)
-            {
-                Console.WriteLine(arr1[i]);
-            }
 
+            Console.WriteLine("Sorted Array :");
+            foreach (int ele in arr)
+            {
+                Console.WriteLine(ele);
+            }
         }
     }
 }
+
+
