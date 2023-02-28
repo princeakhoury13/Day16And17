74f2b18222921b66f85577981b804f8c42cb45f3
princeakhoury13 <princepraveen130509@gmail.com>
2023-02-28 19:05:46 +0530

    added: Creating array for merge sort

--- Program.cs
+++ Program.cs
@@ -4,8 +4,15 @@
     {
         public static void Main(string[] args)
         {
-            Sorting sort = new Sorting();
-            sort.Display();
+            int[] arr = { 52, 4, 300, 21, 102 };
+
+            Console.WriteLine("Array before sorting:");
+            Sorting.PrintArray(arr);
+
+            Sorting.MergeSort(arr, 0, arr.Length - 1);
+
+            Console.WriteLine("\nArray after sorting:");
+            Sorting.PrintArray(arr);
         }
     }
 }
